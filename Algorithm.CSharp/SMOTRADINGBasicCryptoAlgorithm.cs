be430bc5a7e13338717fe332f452506f1998ca1e
Omar MEKNI <mekniomar66@gmail.com>
2023-12-20 21:23:45 +0100

    SMO Trading VF Abdelnaby Mohaned Mekni Omar Laaribi Saida

--- Algorithm.CSharp/SMOTRADINGBasicCryptoAlgorithm.cs
+++ Algorithm.CSharp/SMOTRADINGBasicCryptoAlgorithm.cs
@@ -13,101 +13,159 @@
  * limitations under the License.
 */
 
+using QuantConnect.Indicators;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
+using QuantConnect.Algorithm;
 using QuantConnect.Data;
 using QuantConnect.Brokerages;
-using QuantConnect.Indicators;
-using QuantConnect.Orders;
-using QuantConnect.Interfaces;
 
-namespace QuantConnect.Algorithm.CSharp
+using QuantConnect.Parameters;
+using System.Linq;
+
+using QuantConnect.Algorithm.Framework.Risk;
+using QuantConnect.Algorithm.Framework.Portfolio;
+
+using Accord.Math;
+
+using QuantConnect.Data.Market;
+using QuantConnect.Orders;
+
+namespace QuantConnect
 {
-    /// <summary>
-    /// The demonstration algorithm shows some of the most common order methods when working with Crypto assets.
-    /// </summary>
-    /// <meta name="tag" content="using data" />
-    /// <meta name="tag" content="using quantconnect" />
-    /// <meta name="tag" content="trading and orders" />
     public class SMOTRADINGBasicCryptoAlgorithm : QCAlgorithm
     {
-        private ExponentialMovingAverage _fast;
-        private ExponentialMovingAverage _slow;
+        private RollingWindow<decimal> _priceWindow;
+        private RollingWindow<decimal> _correlationWindow;
+        private const int WindowSize = 14;
+        private const decimal CorrelationThreshold = 0.5m;
 
-        /// <summary>
-        /// Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.
-        /// </summary>
+        [Parameter("macd-fast")]
+        public int FastPeriodMacd = 12;
+
+        [Parameter("macd-slow")]
+        public int SlowPeriodMacd = 26;
+
+        private MovingAverageConvergenceDivergence _macd;
+        private Symbol _btcusd;
+        private bool _invested;
+
+        private string _ChartName = "Trade Plot";
+        private string _PriceSeriesName = "Price";
+        private string _PortfoliovalueSeriesName = "PortFolioValue";
+
+        private BollingerBands _bollingerBands;
+        private TrailingStopRiskManagementModel _trailingStopRiskManagementModel;
+
+        private decimal _supportLevel = 50000m;
+        private decimal _resistanceLevel = 65000m;
+
         public override void Initialize()
         {
-            SetStartDate(2018, 1, 1); // Set Start Date
-            SetEndDate(2018, 12, 31); // Set End Date
-
-            // Although typically real brokerages as GDAX only support a single account currency,
-            // here we add both USD and EUR to demonstrate how to handle non-USD account currencies.
-            // Set Strategy Cash (USD)
+            SetStartDate(2018, 1, 1);
+            SetEndDate(2022, 3, 22);
+            SetBrokerageModel(BrokerageName.Bitstamp, AccountType.Cash);
             SetCash(10000);
 
-            
-            // Add some coins as initial holdings
-            // When connected to a real brokerage, the amount specified in SetCash
-            // will be replaced with the amount in your actual account.
-            SetCash("BTC", 1m);
+            _btcusd = AddCrypto("BTCUSD", Resolution.Daily).Symbol;
+            _macd = MACD(_btcusd, FastPeriodMacd, SlowPeriodMacd, 9, MovingAverageType.Exponential, Resolution.Daily, Field.Close);
 
-            SetBrokerageModel(BrokerageName.GDAX, AccountType.Cash);
+            var stockPlot = new Chart(_ChartName);
+            var assetPrice = new Series(_PriceSeriesName, SeriesType.Line, "$", Color.Blue);
+            var portFolioValue = new Series(_PortfoliovalueSeriesName, SeriesType.Line, "$", Color.Green);
+            stockPlot.AddSeries(new Series("MACD", SeriesType.Line, "$", Color.Yellow));
+            stockPlot.AddSeries(assetPrice);
+            stockPlot.AddSeries(portFolioValue);
+            AddChart(stockPlot);
+            Schedule.On(DateRules.EveryDay(), TimeRules.Every(TimeSpan.FromDays(1)), DoPlots);
 
-            // You can uncomment the following line when live trading with GDAX,
-            // to ensure limit orders will only be posted to the order book and never executed as a taker (incurring fees).
-            // Please note this statement has no effect in backtesting or paper trading.
-            // DefaultOrderProperties = new GDAXOrderProperties { PostOnly = true };
+            _priceWindow = new RollingWindow<decimal>(WindowSize);
+            _correlationWindow = new RollingWindow<decimal>(WindowSize);
 
-            // Find more symbols here: http://quantconnect.com/data
-           
-            var symbol = AddCrypto("BTCUSD").Symbol;
+            _bollingerBands = BB(_btcusd, 20, 2, MovingAverageType.Simple, Resolution.Daily);
+            stockPlot.AddSeries(new Series("bollingerUpperBand", SeriesType.Line, "$", Color.Red));
+            stockPlot.AddSeries(new Series("bollingerlowerBand", SeriesType.Line, "$", Color.Red));
+            AddChart(stockPlot);
 
-            // create two moving averages
-            _fast = EMA(symbol, 30, Resolution.Minute);
-            _slow = EMA(symbol, 60, Resolution.Minute);
+            _trailingStopRiskManagementModel = new TrailingStopRiskManagementModel(0.05m);
+            SetRiskManagement(_trailingStopRiskManagementModel);
         }
 
-        /// <summary>
-        /// OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
-        /// </summary>
-        /// <param name="data">Slice object keyed by symbol containing the stock data</param>
+        private void DoPlots()
+        {
+            Plot(_ChartName, _PriceSeriesName, Securities[_btcusd].Price);
+            Plot(_ChartName, _PortfoliovalueSeriesName, Portfolio.TotalPortfolioValue);
+            Plot(_ChartName, "bollingerUpperBand", _bollingerBands.UpperBand);
+            Plot(_ChartName, "bollingerlowerBand", _bollingerBands.LowerBand);
+            Plot(_ChartName, "macdSignal ", _macd.Signal);
+        }
+
         public override void OnData(Slice data)
         {
-            if (Portfolio.CashBook["BTC"].ConversionRate == 0)
-            {
-                Log($"BTC conversion rate: {Portfolio.CashBook["BTC"].ConversionRate}");
+            if (!_macd.IsReady) return;
 
-                throw new Exception("Conversion rate is 0");
-            }
-            if (Time.Hour == 2 && Time.Minute == 0)
+            var closePrice = Securities[_btcusd].Close;
+            var holdings = Portfolio[_btcusd].Quantity;
+
+            if (holdings <= 0 && _macd > _macd.Signal && _priceWindow.IsReady && closePrice <= _supportLevel)
             {
-                // Submit a buy limit order for BTC at 5% below the current price
-                var usdTotal = Portfolio.CashBook["USD"].Amount;
-                var limitPrice = Math.Round(Securities["BTCUSD"].Price * 0.95m, 2);
-                // use only half of our total USD
-                var quantity = usdTotal * 0.5m / limitPrice;
-                LimitOrder("BTCUSD", quantity, limitPrice);
+                decimal _correlation = Correlation(_priceWindow, WindowSize);
+                if (_correlation > CorrelationThreshold)
+                {
+                    SetHoldings(_btcusd, 1.0);
+                    _invested = true;
+                }
             }
-            else if (Time.Hour == 11 && Time.Minute == 0)
+            else if (_invested && _macd < _macd.Signal && closePrice >= _resistanceLevel)
             {
-                // Liquidate our BTC holdings (including the initial holding)
-                SetHoldings("BTCUSD", 0m);
+                Liquidate(_btcusd);
+                _invested = false;
+            }
+
+            if (!data.ContainsKey(_btcusd)) return;
+
+            _priceWindow.Add(data[_btcusd].Close);
+            decimal correlation = Correlation(_priceWindow, WindowSize);
+            _correlationWindow.Add(correlation);
+
+            var upperBand = _bollingerBands.UpperBand.Current.Value;
+            var lowerBand = _bollingerBands.LowerBand.Current.Value;
+
+            if (data.Bars[_btcusd].Close > upperBand && !_invested)
+            {
+                Liquidate(_btcusd);
+                _invested = false;
+            }
+            else if (data.Bars[_btcusd].Close < lowerBand && !_invested)
+            {
+                SetHoldings(_btcusd, 1.0);
+                _invested = true;
+            }
+
+            var riskAdjustedTargets = _trailingStopRiskManagementModel.ManageRisk(this, new[] { new PortfolioTarget(_btcusd, holdings) });
+            foreach (var target in riskAdjustedTargets)
+            {
+                SetHoldings(target.Symbol, 5);
             }
         }
 
-        public override void OnOrderEvent(OrderEvent orderEvent)
+        private decimal Correlation(RollingWindow<decimal> x, int period)
         {
-            Debug(Time + " " + orderEvent);
-        }
+            if (x.IsReady)
+            {
+                var meanX = x.Average();
+                var meanY = x.Skip(1).Take(period).Average();
+                var cov = x.Zip(x.Skip(1).Take(period), (xi, yi) => (xi - meanX) * (yi - meanY)).Sum();
+                var stdDevX = (decimal)Math.Sqrt(x.Sum(xi => (double)((xi - meanX) * (xi - meanX))));
+                var stdDevY = (decimal)Math.Sqrt(x.Skip(1).Take(period).Sum(yi => (double)((yi - meanY) * (yi - meanY))));
 
-        public override void OnEndOfAlgorithm()
-        {
-            Log($"{Time} - TotalPortfolioValue: {Portfolio.TotalPortfolioValue}");
-            Log($"{Time} - CashBook: {Portfolio.CashBook}");
+                if (stdDevX > 0 && stdDevY > 0)
+                {
+                    return cov / (stdDevX * stdDevY);
+                }
+            }
+
+            return 0m;
         }
-        
     }
 }
